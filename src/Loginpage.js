889ac4b6c727b51bff0b0ad38b9b4da978728b92
seekerpig <laserklet@gmail.com>
2021-05-09 22:56:10 +0800

    updated and commented out unused warnings for deploy

--- src/Loginpage.js
+++ src/Loginpage.js
@@ -5,7 +5,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import {Link as RouterLink, useHistory} from 'react-router-dom';
+import {Link as RouterLink} from 'react-router-dom';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Alert from "@material-ui/lab/Alert";
